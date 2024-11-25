c9a65840bdf865058a0e176f46da35a6cd878ff8
jasurbek <jasurdev1604@gmail.com>
2024-11-25 16:06:19 +0500

    new

--- src/components/Showcase/showcase.jsx
+++ src/components/Showcase/showcase.jsx
@@ -9,7 +9,7 @@ const Showcase = () => {
         <div className="showcase">
           <div className="showcase__title title">
             Aipply Academy - sun'iy intellektga ixtisoslashtirilgan zamonaviy
-            kasblar o'quv markazi!
+            kasblar o'quv markazi!!
           </div>
           <div className="showcase__form">
             <Form />
